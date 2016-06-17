74150917f98275f97f590391aa4bf9a281c6c28d
jinghuazhao <jinghuazhao@users.noreply.github.com>
2016-06-17 09:08:20 +0100

    Delete summary.sh

--- summary.sh
+++
@@ -1,8 +0,0 @@
-#16-6-2016 MRC-Epid JHZ
-
-grep -H gene bmd/twas/*imp | grep gene_exp | awk '!/nan/' \
-|sed 's\bmd/twas/\\g'|sed 's\:gene_exp\\g'|sed 's\.imp\\g' > bmd.imp
-    
-awk '($1=="ARG2"||$1=="KCNJ13")' bmd.imp
-
-  
