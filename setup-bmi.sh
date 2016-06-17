2d05bdd941025a34cd7ed828190cacd31d4d5fca
jinghuazhao <jinghuazhao@users.noreply.github.com>
2016-06-17 10:41:16 +0100

    Update setup-bmi.sh

--- setup-bmi.sh
+++ setup-bmi.sh
@@ -10,6 +10,7 @@ wget http://www.broadinstitute.org/collaboration/giant/images/f/f0/All_ancestrie
 ln -sf SNP_gwas_mc_merge_nogc.tbl.uniq.gz BMI-EUR.gz
 ln -sf All_ancestries_SNP_gwas_mc_merge_nogc.tbl.uniq.gz BMI-ALL.gz
 
-gunzip -c BMI-EUR.gz | awk '(NR>1)' | sort -t$'\t' -k1,1 > t.txt
-awk '(NR>1)' /gen_omics/data/3-2012/legend.txt | sort -t$'\t' -k1,1 | awk '($1!=".")'> snp.txt
-join -j 1 -t$'\t' snp.txt t.txt | sort -t$'\t' -n -k2,2n -k3,3n > BMI-EUR.txt
+# The following is to add SNP positionss
+# gunzip -c BMI-EUR.gz | awk '(NR>1)' | sort -t$'\t' -k1,1 > t.txt
+# awk '(NR>1)' /gen_omics/data/3-2012/legend.txt | sort -t$'\t' -k1,1 | awk '($1!=".")'> snp.txt
+# join -j 1 -t$'\t' snp.txt t.txt | sort -t$'\t' -n -k2,2n -k3,3n > BMI-EUR.txt
