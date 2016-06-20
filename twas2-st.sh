472b3d7536777330f0d11f9ee714e2ec7e546889
jinghuazhao <jinghuazhao@users.noreply.github.com>
2016-06-20 14:36:35 +0100

    Collection of results for the GIANT example

--- twas2-st.sh
+++ twas2-st.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# 17-6-2016 MRC-Epid JHZ
+# 20-6-2016 MRC-Epid JHZ
 
 # seq 22 | parallel -j12 /genetics/data/CGI/TWAS-pipeline/bmi.sh {}
 # seq 22 | parallel -j12 /genetics/data/CGI/TWAS-pipeline/bmi-twas2.sh {}
@@ -26,4 +26,4 @@ done
 # gunzip -c $rt/bmi/BMI-EUR.gz | awk '(NR>1){FS=OFS="\t";print $1, $2, $3, $5/$6}' | sort -t$'\t' -k1,1 > bmi.txt
 # then BMI replaces ALL EUR below
 
-parallel -j8 -S b01,b02,b03,b04,b05,b06,b07,b08 /genetics/data/CGI/TWAS-pipeline/twas2.sh {1} {2} {3} ::: bmi.txt ::: ALL EUR ::: MET NTR YFS ::: $(seq 1000) 
+parallel -j8 -S b01,b02,b03,b04,b05,b06,b07,b08 /genetics/data/CGI/TWAS-pipeline/twas2.sh {1} {2} {3} {4} ::: bmi.txt ::: ALL EUR ::: MET NTR YFS ::: $(seq 1000) 
