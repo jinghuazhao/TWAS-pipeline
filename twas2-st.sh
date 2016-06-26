7b4e2230f943512c3319b3a4621a072105493381
jinghuazhao <jinghuazhao@users.noreply.github.com>
2016-06-26 23:57:59 +0100

    Parallel computing with twas2.sh

--- twas2-st.sh
+++ twas2-st.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-# 20-6-2016 MRC-Epid JHZ
+# 26-6-2016 MRC-Epid JHZ
 
 parallel -j8 -S b01,b02,b03,b04,b05,b06,b07,b08 /genetics/data/CGI/TWAS-pipeline/twas2.sh {1} {2} {3} {4} ::: bmi.txt ::: ALL EUR ::: MET NTR YFS ::: $(seq 1000) 
