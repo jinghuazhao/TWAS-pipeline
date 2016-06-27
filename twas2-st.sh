938738c118662febb0a5dd78e03b8cde8867aab8
jinghuazhao <jinghuazhao@users.noreply.github.com>
2016-06-27 12:35:22 +0100

    Paralllel computing with twas2

---
+++ twas2-st.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# 27-6-2016 MRC-Epid JHZ
+
+S=$1
+dir=$2
+TWAS=/genetics/bin/TWAS
+TWAS2=/genetics/bin/TWAS-pipeline
+
+parallel -j8 -S $S twas2.sh {1} {2} {3} {4} {5} ::: $TWAS ::: $TWAS2 ::: $dir ::: MET NTR YFS ::: $(seq 1000) 
