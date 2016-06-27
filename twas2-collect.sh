38b6441b2817f7964e8a034f490df11aa332ea2c
jinghuazhao <jinghuazhao@users.noreply.github.com>
2016-06-27 10:17:15 +0100

    Collection of results for imputation

--- twas2-collect.sh
+++ twas2-collect.sh
@@ -1,6 +1,5 @@
-# 23-6-2016 MRC-Epid JHZ
+# 27-6-2016 MRC-Epid JHZ
 
-# source (EUR, ALL)
 src=$1
 
 # grep -H gene $src/*/*imp | grep gene_exp | awk '!/nan/{gsub(/.imp|:gene_exp/,"",$0);print}'|sed 's\/\ \g' > bmi-$src.imp
