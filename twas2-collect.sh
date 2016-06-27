3653afc86dbdd92041970d931f81e90ab0734894
jinghuazhao <jinghuazhao@users.noreply.github.com>
2016-06-27 10:17:58 +0100

    Collection of results for imputation

--- twas2-collect.sh
+++ twas2-collect.sh
@@ -2,6 +2,7 @@
 
 src=$1
 
-# grep -H gene $src/*/*imp | grep gene_exp | awk '!/nan/{gsub(/.imp|:gene_exp/,"",$0);print}'|sed 's\/\ \g' > bmi-$src.imp
 grep -H gene $src/*/*imp | grep gene_exp | awk '!/nan/' | sed -e 's/.imp//g;s/:gene_exp//g;s\/\ \g' > $src.imp
 
+# alternative form
+# grep -H gene $src/*/*imp | grep gene_exp | awk '!/nan/{gsub(/.imp|:gene_exp/,"",$0);print}'|sed 's\/\ \g' > bmi-$src.imp
