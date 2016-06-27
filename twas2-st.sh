553c3290078025f260316939c812989f2c4de2b6
jinghuazhao <jinghuazhao@users.noreply.github.com>
2016-06-27 07:03:15 +0100

    Paralllel computing with twas2

--- twas2-st.sh
+++ twas2-st.sh
@@ -5,4 +5,4 @@ S=$1
 zfile=$2
 dir=$3
 
-parallel -j8 -S $S /genetics/data/CGI/TWAS-pipeline/twas2.sh {1} {2} {3} {4} ::: $zfile ::: $dir ::: MET NTR YFS ::: $(seq 1000) 
+parallel -j8 -S $S twas2.sh {1} {2} {3} {4} ::: $zfile ::: $dir ::: MET NTR YFS ::: $(seq 1000) 
