84d678816a84030a9ba1f5b532a82e58a6ea32ee
jinghuazhao <jinghuazhao@users.noreply.github.com>
2016-06-27 15:58:06 +0100

    Delete twas2-collect.sh

--- twas2-collect.sh
+++
@@ -1,8 +0,0 @@
-# 27-6-2016 MRC-Epid JHZ
-
-src=$1
-
-grep -H gene $src/*/*imp | grep gene_exp | awk '!/nan/' | sed -e 's/.imp//g;s/:gene_exp//g;s\/\ \g' > $src.imp
-
-# alternative form
-# grep -H gene $src/*/*imp | grep gene_exp | awk '!/nan/{gsub(/.imp|:gene_exp/,"",$0);print}'|sed 's\/\ \g' > $src.imp
