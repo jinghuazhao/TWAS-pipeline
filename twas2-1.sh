615308731de9fed726becdd275b076f884178f35
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2016-06-29 14:19:23 +0100

    Imputation for a single gene

--- twas2-1.sh
+++ twas2-1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 27-6-2016 MRC-Epid JHZ
+# 29-6-2016 MRC-Epid JHZ
 
 TWAS=$1
 TWAS2=$2
@@ -15,7 +15,7 @@ awk -vgs=$gs '($1==gs)' $TWAS/$pop.lst | awk -vTWAS=$TWAS -vTWAS2=$TWAS2 -vpop=$
 # zscore setup
     system(sprintf("echo SNP_ID SNP_Pos Ref_Allele Alt_Allele Z-score > %s.zscore",gene))
     system(sprintf("sort -k1,1 %s.map > %s",filename,gene))
-    system(sprintf("join -j 1 %s gwas2.txt > %s.join",gene,gene))
+    system(sprintf("join -j 1 %s tgwas2.txt > %s.join",gene,gene))
     system(sprintf("sort -k2,2n %s.join > %s.sort",gene,gene))
     system(sprintf("awk -f %s/twas2-swap.awk %s.sort >> %s.zscore",TWAS2,gene,gene))
 # imputation
