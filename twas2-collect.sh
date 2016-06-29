a25960c4a2aefd250e23e42881f1a494186b20a0
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2016-06-29 15:12:47 +0100

    Collection of results from imputation

--- twas2-collect.sh
+++ twas2-collect.sh
@@ -3,8 +3,8 @@
 
 src=$1
 
-echo "Source Pop SNP_name SNP_pos Ref_Allele Alt_Allele Z-Score r2pred" > $src.imp
-grep -H gene $src/*/*imp | grep gene_exp | awk '!/nan/' | sed -e 's/.imp//g;s/:gene_exp//g;s\/\ \g' >> $src.imp
+echo "Pop Gene Z_Score r2pred" > $src.imp
+grep -H gene $src/*/*imp | grep gene_exp | awk '!/nan/' | sed -e 's/.imp//g;s/:gene_exp//g;s\/\ \g' | awk '{print $2,$3,$7,$8}'>> $src.imp
 
 # alternative form
 # grep -H gene $src/*/*imp | grep gene_exp | awk '!/nan/{gsub(/.imp|:gene_exp/,"",$0);print}'|sed 's\/\ \g' > $src.imp
