4ce67bfc1ed5c3433d04e731d64ef498c6d193b4
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2016-08-29 14:06:43 +0100

    Single node version

--- twas-single.sh
+++ twas-single.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 17-8-2016 MRC-Epid JHZ
+# 29-8-2016 MRC-Epid JHZ
 
 if [ $# -lt 1 ] || [ "$1" == "-h" ]; then
     echo "Usage: twas-single.sh <input>"
@@ -19,7 +19,7 @@ awk ' (NR>1) {
   $2=toupper($2)
   $3=toupper($3)
   print $1, $2, $3, $4, $5/$6
-}' $1 | sort -k1,1 > $1.input
+}' $1 | sort -k1,1 > $(basename $1).input
 echo Step 3 - make population-specific twas2.txt with cleaned Z-scores
 dir=`pwd`/$(basename $1).tmp
 for pop in MET NTR YFS
@@ -27,12 +27,17 @@ do
   if [ ! -d $dir/$pop ]; then
      mkdir -p $dir/$pop
   fi
-  join -1 2 -2 1 $TWAS/$pop.bim $1.input | awk -f $TWAS/CLEAN_ZSCORES.awk | awk '{$2="";print}' > $dir/$pop/twas2.txt
+  join -1 2 -2 1 $TWAS/$pop.bim $(basename $1).input | awk -f $TWAS/CLEAN_ZSCORES.awk | awk '{$2="";print}' > $dir/$pop/twas2.txt
 done
 echo Step 4 - perform analysis
 parallel -j8 twas2.sh {1} {2} {3} {4} {5} ::: $TWAS ::: $TWAS2 ::: $dir ::: MET NTR YFS ::: $(seq 1000)
 echo Step 5 - collect results
 twas2-collect.sh $(basename $1).tmp
 echo Step 6 - tidy up
-mv $(basename $1).tmp.imp $(basename $1).imp
-rm -rf $dir $1.input
+awk '{
+  if(NR==1) print $0 " p"
+  else {
+    printf $0 " ";system(sprintf("pnorm %lf",$3))
+  }
+}' $(basename $1).tmp.imp > $(basename $1).imp
+rm -rf $dir $(basename $1).input $(basename $1).tmp.imp
