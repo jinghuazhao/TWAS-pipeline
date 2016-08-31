a55712ee12ab1463c163fbe2defb6e0cd5e5386c
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2016-08-31 11:02:21 +0100

    Single node version

--- twas-single.sh
+++ twas-single.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 30-8-2016 MRC-Epid JHZ
+# 31-8-2016 MRC-Epid JHZ
 
 if [ $# -lt 1 ] || [ "$1" == "-h" ]; then
     echo "Usage: twas-single.sh <input>"
@@ -34,10 +34,34 @@ parallel -j8 twas2.sh {1} {2} {3} {4} {5} ::: $TWAS ::: $TWAS2 ::: $dir ::: MET 
 echo Step 5 - collect results
 twas2-collect.sh $(basename $1).tmp
 echo Step 6 - tidy up
-awk '{
+awk '
+function abs(x)
+{
+  if (x<0) x=-x
+  return x
+}
+function cPhi(x)
+{
+  R[0]=1.25331413731550025
+  R[1]=.421369229288054473
+  R[2]=.236652382913560671
+  R[3]=.162377660896867462
+  R[4]=.123131963257932296
+  R[5]=.0990285964717319214
+  R[6]=.0827662865013691773
+  R[7]=.0710695805388521071
+  R[8]=.0622586659950261958
+  j=int(.5*(abs(x)+1));
+  pwr=1;a=R[j];z=2*j;b=a*z-1;h=abs(x)-z;s=a+h*b;t=a;q=h*h;
+  for(i=2;s!=t;i+=2) {a=(a+z*b)/i; b=(b+z*a)/(i+1); pwr*=q; s=(t=s)+pwr*(a+h*b);}
+  s=s*exp(-.5*x*x-.91893853320467274178);
+  return 2*s;
+}
+{
   if(NR==1) print $0 " p"
   else {
-    printf $0 " ";system(sprintf("pnorm %lf",$3))
+    print $0 " "  cPhi($3)
+#   printf $0 " ";system(sprintf("pnorm %lf",$3))
   }
 }' $(basename $1).tmp.imp | awk '{t=$5;$5=$4;$4=t;print}' > $(basename $1).imp
 rm -rf $dir $(basename $1).input $(basename $1).tmp.imp
