fa69319c0bc84c11b27eee162cddbf17e91a9056
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2016-09-16 15:04:20 +0100

    EWAS imputation

--- ewas.sh
+++ ewas.sh
@@ -7,8 +7,8 @@ TWAS2=/genetics/bin/TWAS-pipeline
 echo Step 2 - reformat input
 awk ' (NR>1) {
   FS=OFS="\t"
-  $2=toupper($2)
   $3=toupper($3)
+  $4=toupper($4)
   print $1, $2, $3, $4, $5/$6
 }' $1 | sort -k1,1 > $(basename $1).input
 echo Step 3 - make twas2.txt with cleaned Z-scores
