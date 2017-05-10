634d5b5d41a5a128ab20733982d9cd1e576b489a
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-05-10 16:30:46 +0100

    driver

--- gtex-fusion.sh
+++ gtex-fusion.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#1-5-2017 MRC-Epid JHZ
+#10-5-2017 MRC-Epid JHZ
 
 #parallel '/bin/echo {1} {2} /genetics/bin/FUSION/GTEx/{1}' ::: $(/bin/cat ../GTEx.list) ::: $(seq 22)>../GTEx.runlist
 
 if [ $# -lt 1 ] || [ "$1" == "-h" ]; then
-    echo "Usage: gtex-sge.sh <input>"
+    echo "Usage: gtex-fusion.sh <input>"
     echo "where <input> is in tab-delimited format:"
     echo "SNP A1 A2 Z N"
     echo "The output is contained in <$1.tmp>"
