1fdd716b04785657460961f1cede493cec94f7d3
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-07-13 16:46:46 +0100

    utilities

--- fusion.sh
+++ fusion.sh
@@ -3,11 +3,11 @@
 TWAS=/genetics/bin/TWAS-pipeline
 for i in $(cut -d ' ' -f1 $TWAS/GE.runlist|uniq)
 do
-  echo "$1/$i"
+  echo "$1/$i:"
   fusion.R $1.GE $i
 done
 for i in $(cut -d ' ' -f1 $TWAS/GTEx.runlist|sed 's/GTEx\.//g'|uniq)
 do
-  echo "$1/$i"
+  echo "$1/$i:"
   fusion.R $1.GTEx $i
 done
