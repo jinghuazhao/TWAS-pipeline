1244d13816a47b40960466662c96676527c5747e
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-07-13 08:59:08 +0100

    FUSION using GE

---
+++ ge-fusion.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#13-7-2017 MRC-Epid JHZ
+
+if [ $# -lt 1 ] || [ "$1" == "-h" ]; then
+    echo "Usage: gtex-fusion.sh <input>"
+    echo "where <input> is in tab-delimited format:"
+    echo "SNP A1 A2 Z N"
+    echo "The output is contained in <$1.tmp>"
+    exit
+fi
+dir=$(pwd)/$(basename $1).GE
+if [ ! -d $dir ]; then
+   mkdir -p $dir
+fi
+FUSION=/genetics/bin/FUSION/tests
+ln -sf $FUSION/glist-hg19 $dir/glist-hg19
+TWAS=/genetics/bin/TWAS-pipeline
+qsub -cwd -sync y -v FUSION=$TWAS -v dir=$dir -v sumstats=$(pwd)/$1 -v GTEx=/genetics/bin/FUSION/GTEx -v TWAS=$TWAS $TWAS/ge-fusion.qsub
